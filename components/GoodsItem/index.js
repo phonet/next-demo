898472be96648a3625b03b2c1c9df29fe2c60e7f
wb-tf707260 <wb-tf707260@alibaba-inc.com>
2020-05-28 03:10:02 +0800

    分类商品列表页面

--- components/GoodsItem/index.js
+++ components/GoodsItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './index.less';
+import ShoppingCartOutlined from "@ant-design/icons/lib/icons/ShoppingCartOutlined";
 
 /**
  * 商品项展示
@@ -7,12 +8,32 @@ import styles from './index.less';
  * @returns {*}
  * @constructor
  */
-const GoodsItem = () => {
-	return (
-		<div>
-			商品项展示
-		</div>
-	);
+const GoodsItem = ({
+                       myClassName
+                   }) => {
+    return (
+        <a href=""
+           className={`${styles.goodsItem} ${myClassName ? myClassName : ''}`}
+            //style={tempStyle}
+        >
+            <img src="../../static/images/test/goods.jpg" className={styles.goodsImg} alt=""/>
+            <div className={styles.goodsDesc}>
+                <p className={styles.goodsName}>Kiehl's 科颜氏 金盏花植物精华水金盏花植物精华</p>
+                <p className={styles.priceWrap}>
+                    <span className={styles.nowPrice}>¥519</span>
+                    <span className={styles.marketPrice}>市场价 ¥630</span>
+                </p>
+                <p className={styles.storeName}>春雨（papa recipe）海外旗舰店</p>
+                <div className={`${styles.totalSaleWrap} fcb`}>
+                    <p className={`${styles.saleTotal} fl fcb`}>
+                        销量：<span className={styles.num}>85万</span>
+                    </p>
+                    <span className={`${styles.car} fr`}><ShoppingCartOutlined
+                        style={{fontSize: 24, color: '#F33A3F'}}/></span>
+                </div>
+            </div>
+        </a>
+    );
 };
 
 export default GoodsItem;
--- components/GoodsList/index.js
+++ components/GoodsList/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './index.less'
+import GoodsItem from "../GoodsItem";
 
 /**
  * 商品列表
@@ -7,11 +9,18 @@ import React from 'react';
  * @constructor
  */
 const GoodsList = () => {
-	return (
-		<div>
-			商品列表
-		</div>
-	);
+    const testArr = new Array(40).fill(1)
+    return (
+        <div className={`${styles.listWrap} contentWidth fcb`}>
+            {
+                testArr.map((o, i) => {
+                    return (
+                        <GoodsItem key={i}/>
+                    )
+                })
+            }
+        </div>
+    );
 };
 
 export default GoodsList;
